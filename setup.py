eb929a1278a1fd89c9c2a026920c1b2fbfce243e
fdpeiter <fdpeiter@gmail.com>
2016-11-15 22:48:19 -0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='arp-simulator',
+    name='net-simulator',
     version='0.0.1',
-    description='Simple simulator for arp requests',
+    description='Simple simulator for Echo and ARP requests',
     long_description=readme,
     author='Felipe Peiter',
     author_email='Felipe Peiter',
-    url='https://github.com/fdpeiter/arp-simulator',
+    url='https://github.com/fdpeiter/simple-net-simulator',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
